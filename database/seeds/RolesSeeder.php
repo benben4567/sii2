5aa87b5f68ae48cedb999f6be0e9138426bac6fa
benben4567 <benben4567@gmail.com>
2020-10-28 07:33:59 +0700

    initial commit

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Contracts\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        //create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'super-admin']);
+        $role2 = Role::create(['name' => 'instruktur']);
+
+        //create user and assign role
+        $user = User::create([
+          'username' => 'superadmin',
+          'email' => 'superadmin@example.com',
+          'password' => Hash::make('secret')
+        ]);
+        $user->assignRole($role1);
+
+        $user = User::create([
+          'username' => '8511440Z',
+          'email' => 'ihwanda.agus@pln.co.id',
+          'password' => Hash::make('password')
+        ]);
+        $user->assignRole($role2);
+    }
+}
