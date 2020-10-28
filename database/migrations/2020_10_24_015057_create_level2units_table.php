cc3123da6096aaf722f38b0640ae7db3f2cf084b
benben4567 <benben4567@gmail.com>
2020-10-28 18:14:45 +0700

    remove unique

--- database/migrations/2020_10_24_015057_create_level2units_table.php
+++ database/migrations/2020_10_24_015057_create_level2units_table.php
@@ -16,7 +16,7 @@ class CreateLevel2UnitsTable extends Migration
         Schema::create('level2units', function (Blueprint $table) {
             $table->bigIncrements('id')->unsigned();
             $table->foreignId('level1unit_id')->constrained();
-            $table->string('kode_unit_level2', 5)->unique();
+            $table->string('kode_unit_level2', 5);
             $table->string('unit_level2', 255);
             $table->integer('dibuat_oleh')->unsigned();
             $table->integer('diedit_oleh')->unsigned();
