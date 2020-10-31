208beb0a5567376f0bbb39e71d464f17ac1464c8
benben4567 <benben4567@gmail.com>
2020-10-31 09:51:28 +0700

    add method getData

--- app/Http/Controllers/InstrukturController.php
+++ app/Http/Controllers/InstrukturController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use DataTables;
 
 class InstrukturController extends Controller
 {
     public function index()
     {
       return view('page.instruktur.index');
+    }
+
+    public function getData(Request $request)
+    {
+      if ($request->ajax()) {
+        $instruktur = DB::table('instrukturs')
+                    ->join('pesertas', 'pesertas.id', "=", "instrukturs.peserta_id")
+                    ->join('tipeinstrukturs', 'tipeinstrukturs.id', "=", "instrukturs.tipeinstruktur_id")
+                    ->select('nama', 'nip', 'tipe_instruktur', 'jabatan', 'email')
+                    ->get();
+        return DataTables::of($instruktur)
+                    ->addIndexColumn()
+                    ->make(true);
+      }
     }
 
     public function show()
