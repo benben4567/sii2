440193e565080344c3787508610e5b51a467062b
Benben <benben4567@gmail.com>
2020-11-05 07:10:24 +0700

    fix query

--- app/Http/Controllers/JudulController.php
+++ app/Http/Controllers/JudulController.php
@@ -24,20 +24,26 @@ class JudulController extends Controller
   public function getData(Request $request)
   {
     if ($request->ajax()) {
-      $judul = DB::table('juduls')
-        ->join('jenisdiklats', 'jenisdiklats.id', "=", "juduls.jenisdiklat_id")
-        ->join('sifatdiklats', 'sifatdiklats.id', "=", "juduls.sifatdiklat_id")
-        ->join('dahanprofesis', 'dahanprofesis.id', "=", 'juduls.dahanprofesi_id')
-        ->join('levelprofisiensis', 'levelprofisiensis.id', '=', 'juduls.levelprofisiensi_id')
-        ->join('penyelenggaraans', 'penyelenggaraans.id', '=', 'juduls.penyelenggaraan_id')
-        ->join('jenissertifikats', 'jenissertifikats.id', '=', 'juduls.jenissertifikat_id')
-        ->join('penanggungjawabs', 'penanggungjawabs.id', "=", "juduls.penanggungjawab_id")
-        ->select('juduls.*', 'jenis_diklat', 'sifat_diklat', 'dahan_profesi', 'level_profisiensi', 'penyelenggaraan', 'penanggung_jawab', 'jenis_Sertifikat')
-        ->get();
+      $judul = \App\Judul::select('id','nama_judul', 'sifatdiklat_id', 'jenisdiklat_id' )
+                ->with(['warnings:id,judul_id','sifatdiklat:id,sifat_diklat','jenisdiklat:id,jenis_diklat'])
+                ->get();
+      // $judul = \App\Judul::with(['warnings','sifatdiklat','jenisdiklat'])->get();
+      // $judul = DB::table('juduls')
+      //   ->join('jenisdiklats', 'jenisdiklats.id', "=", "juduls.jenisdiklat_id")
+      //   ->join('sifatdiklats', 'sifatdiklats.id', "=", "juduls.sifatdiklat_id")
+      //   ->join('dahanprofesis', 'dahanprofesis.id', "=", 'juduls.dahanprofesi_id')
+      //   ->join('levelprofisiensis', 'levelprofisiensis.id', '=', 'juduls.levelprofisiensi_id')
+      //   ->join('penyelenggaraans', 'penyelenggaraans.id', '=', 'juduls.penyelenggaraan_id')
+      //   ->join('jenissertifikats', 'jenissertifikats.id', '=', 'juduls.jenissertifikat_id')
+      //   ->join('penanggungjawabs', 'penanggungjawabs.id', "=", "juduls.penanggungjawab_id")
+      //   ->rightJoin('warnings', 'warnings.judul_id', "=", "juduls.id")
+      //   ->select('juduls.*', 'jenis_diklat', 'sifat_diklat', 'dahan_profesi', 'level_profisiensi', 'penyelenggaraan', 'penanggung_jawab', 'jenis_Sertifikat')
+      //   ->get();
 
       // return dd($judul);
       return DataTables::of($judul)
         ->addIndexColumn()
+        // ->toJson();
         ->make(true);
     }
   }
